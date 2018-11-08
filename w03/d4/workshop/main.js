8a836a1ef0d66b68ee2f97120e83d6a672a3fcfa
Dakwon Smith <dsmith@nyc.yearup.org>
2018-11-08 16:51:46 -0500

    Change Background

--- w03/d4/workshop/main.js
+++ w03/d4/workshop/main.js
@@ -1,3 +1,12 @@
 //This comes from data.js
-var gifs = window.data.data;
+let gifs = window.data.data;
+  console.log(gifs);
 
+function changeGifBg(){
+  let gifElement = document.getElementById('background');
+  let randomGif = Math.floor(Math.random() * gifs.length);
+  gifElement.src = gifs.[randomGif]images.orignal.url;
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click',randomGif);
