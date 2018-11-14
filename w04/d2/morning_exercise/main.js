e381b53d42bf3ba0fe34712299550bb213686fcc
Dakwon Smith <dsmith@nyc.yearup.org>
2018-11-14 13:45:33 -0500

    Something

---
+++ w04/d2/morning_exercise/main.js
@@ -0,0 +1,9 @@
+//make a variable for what im going to change
+//select the
+//make a function that will be carried out
+function bgSwitch(userInput){
+//have it set to change the background to the users color
+  document.body.style.backgroundColor = userInput;
+}
+
+button.addEventListener('onclick', bgSwitch)
